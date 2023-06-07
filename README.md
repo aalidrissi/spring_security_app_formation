86dbb071e2106f817a1b227f8df35dc870c79f33
aelidrissi <akram.elidrissi@dxc.com>
2023-06-07 16:33:46 +0100

    Commit d'initialisation repo formation

---
+++ README.md
@@ -0,0 +1,16 @@
+# spring-security
+This is a practice project created to learn the basics of spring security using spring boot framework.
+
+### Developer Instructions:
+##### Requirements:
+- JDK 17
+- Maven 3x
+
+##### Build Using:
+- `mvn package`
+
+### Owner
+- [Bhumika Sharma](https://www.linkedin.com/in/bhumika-sharma-4b07465a/)
+
+### Contributors:
+- [Bhumika Sharma](https://www.linkedin.com/in/bhumika-sharma-4b07465a/)
---
+++ src/main/java/io/springlearnings/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package io.springlearnings.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails normalUser = User.withUsername("bhumika")
+                                     .password(passwordEncoder().encode("password"))
+                                     .roles("NORMAL")
+                                     .build();
+
+        UserDetails adminUser = User.withUsername("bhumika1")
+                                     .password(passwordEncoder().encode("password"))
+                                     .roles("ADMIN")
+                                     .build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+            .authorizeHttpRequests()
+            /*.requestMatchers("home/admin")
+            .hasRole("ADMIN")
+            .requestMatchers("home/normal")
+            .hasRole("NORMAL")*/
+            .requestMatchers("/home/public")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin();
+
+        return httpSecurity.build();
+    }
+
+}
